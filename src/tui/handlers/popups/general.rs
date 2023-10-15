9a593fbb282e51f8e91df5af00d14cfff394d377
bubalooshi <bubalooshi@gmail.com>
2023-10-15 12:17:00 -0400

    Fix clippy suggestion in general popup.

--- src/tui/handlers/popups/general.rs
+++ src/tui/handlers/popups/general.rs
@@ -1,12 +1,10 @@
 use crate::tui::app::{App, AppResult};
-use crossterm::event::{KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+use crossterm::event::{KeyCode, KeyEvent};
 
 pub fn handle_key_events(key_event: KeyEvent, app: &mut App<'_>) -> AppResult<()> {
-    match key_event.code {
-        KeyCode::Enter => {
-            app.dismiss_popup();
-        }
-        _ => (),
+    if key_event.code == KeyCode::Enter {
+        app.dismiss_popup();
     }
+
     Ok(())
 }
