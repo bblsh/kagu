04c8c9d37c8c9de0dbe348b0f0e9990c4b5e4667
bubalooshi <bubalooshi@gmail.com>
2023-12-29 22:41:17 -0500

    Refactor personal screen.

--- tui/src/screens/personal.rs
+++ tui/src/screens/personal.rs
@@ -1,33 +1,36 @@
 use crate::app::{App, KaguFormatting, Pane};
+use ratatui::prelude::*;
 use ratatui::{
     layout::{Constraint, Direction, Layout},
-    prelude::Alignment,
     style::{Color, Modifier, Style},
-    text::Span,
     widgets::{Block, Borders, List, ListItem, Paragraph},
     Frame,
 };
 
 pub fn render(app: &mut App, frame: &mut Frame<'_>) {
-    let top_and_bottom_layout = Layout::default()
+    let [kagu_bar_area, bottom_area] = *Layout::default()
         .direction(Direction::Vertical)
         .margin(0)
-        .constraints([Constraint::Max(1), Constraint::Max(frame.size().width - 1)].as_ref())
-        .split(frame.size());
+        .constraints([Constraint::Max(1), Constraint::Max(frame.size().width - 1)])
+        .split(frame.size())
+    else {
+        return;
+    };
 
-    let kagu_bar = Layout::default()
-        .direction(Direction::Horizontal)
-        .margin(0)
-        .constraints(
-            [
+    let [kagu_logo_area, kagu_voice_status_area, _kagu_spacer_area, kagu_time_area] =
+        *Layout::default()
+            .direction(Direction::Horizontal)
+            .margin(0)
+            .constraints([
                 Constraint::Max(10),                                // Kagu logo
                 Constraint::Max(20),                                // Voice status
                 Constraint::Max(frame.size().width - 10 - 20 - 15), // Blank space
                 Constraint::Max(15),                                // Current time
-            ]
-            .as_ref(),
-        )
-        .split(top_and_bottom_layout[0]);
+            ])
+            .split(kagu_bar_area)
+    else {
+        return;
+    };
 
     let kagu_logo = Paragraph::new("Kagu");
     let time = Paragraph::new(app.get_current_time_string()).alignment(Alignment::Right);
@@ -35,28 +38,22 @@ pub fn render(app: &mut App, frame: &mut Frame<'_>) {
         true => Span::styled("Voice connected", Style::default().fg(Color::LightGreen)),
         false => Span::styled("Voice off", Style::default()),
     });
-    frame.render_widget(kagu_logo, kagu_bar[0]);
-    frame.render_widget(connected_label, kagu_bar[1]);
-    frame.render_widget(time, kagu_bar[3]);
+    frame.render_widget(kagu_logo, kagu_logo_area);
+    frame.render_widget(connected_label, kagu_voice_status_area);
+    frame.render_widget(time, kagu_time_area);
 
-    let back_panel = Layout::default()
+    let [realms_area, middle_panel_area, right_panel_area] = *Layout::default()
         .direction(Direction::Horizontal)
         .margin(0)
-        .constraints(
-            [
-                Constraint::Max(10),
-                Constraint::Max(20),
-                Constraint::Max(frame.size().width - 10 - 20),
-            ]
-            .as_ref(),
-        )
-        .split(top_and_bottom_layout[1]);
-
-    let left_panel = Layout::default()
-        .direction(Direction::Vertical)
-        .margin(0)
-        .constraints([Constraint::Max(3), Constraint::Max(frame.size().height - 4)].as_ref())
-        .split(back_panel[0]);
+        .constraints([
+            Constraint::Max(10),
+            Constraint::Max(20),
+            Constraint::Max(frame.size().width - 10 - 20),
+        ])
+        .split(bottom_area)
+    else {
+        return;
+    };
 
     let realms_list: Vec<ListItem> = app
         .realms
@@ -75,27 +72,33 @@ pub fn render(app: &mut App, frame: &mut Frame<'_>) {
         )
         .highlight_style(Style::default().add_modifier(Modifier::BOLD))
         .highlight_symbol(">");
-    frame.render_stateful_widget(realms, left_panel[1], &mut app.realms.state);
+    frame.render_stateful_widget(realms, realms_area, &mut app.realms.state);
 
-    let middle_panel = Layout::default()
+    let [friends_btn_area, dm_list_area] = *Layout::default()
         .direction(Direction::Vertical)
         .margin(0)
-        .constraints([Constraint::Max(3), Constraint::Max(frame.size().height - 3)].as_ref())
-        .split(back_panel[1]);
+        .constraints([Constraint::Max(3), Constraint::Max(frame.size().height - 3)])
+        .split(middle_panel_area)
+    else {
+        return;
+    };
 
-    let right_panel = Layout::default()
+    let [friends_name_area, dm_history_area] = *Layout::default()
         .direction(Direction::Vertical)
         .margin(0)
-        .constraints([Constraint::Max(3), Constraint::Max(frame.size().height - 3)].as_ref())
-        .split(back_panel[2]);
+        .constraints([Constraint::Max(3), Constraint::Max(frame.size().height - 3)])
+        .split(right_panel_area)
+    else {
+        return;
+    };
 
     let test_top_bar =
         Paragraph::new("Friends or friend_name").block(Block::default().borders(Borders::ALL));
     let test_dm_chat =
         Paragraph::new("DM history here").block(Block::default().borders(Borders::ALL));
 
-    frame.render_widget(test_top_bar, right_panel[0]);
-    frame.render_widget(test_dm_chat, right_panel[1]);
+    frame.render_widget(test_top_bar, friends_name_area);
+    frame.render_widget(test_dm_chat, dm_history_area);
 
     let test_friends_button =
         Paragraph::new("Friends btn").block(Block::default().borders(Borders::ALL));
@@ -105,6 +108,6 @@ pub fn render(app: &mut App, frame: &mut Frame<'_>) {
             .borders(Borders::ALL),
     );
 
-    frame.render_widget(test_friends_button, middle_panel[0]);
-    frame.render_widget(test_friends_list, middle_panel[1]);
+    frame.render_widget(test_friends_button, friends_btn_area);
+    frame.render_widget(test_friends_list, dm_list_area);
 }
